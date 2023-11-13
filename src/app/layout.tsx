e515336fbad9d654316c8e633874e1a295cdd2a4
Luke <lrsorrenti@hotmail.com>
2023-11-13 09:29:12 -0600

    Added dark mode toggle button for switching between themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
 import './globals.css'
+import { ModeToggle } from '@/components/mode-toggle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,12 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <nav>
+          <ModeToggle></ModeToggle>
           <h1 className='flex justify-center'>
             Tasty Recipes
           </h1>
         </nav>
         {children}
+        </ThemeProvider>
       </body>
     </html>
   )
