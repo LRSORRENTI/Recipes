7bf070dc805f64e84224a455e897df35d92ebf71
Luke <lrsorrenti@hotmail.com>
2023-11-14 08:12:25 -0600

    changed the tailwind classes for responsiveness in the main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,8 +36,41 @@ export default async function Home() {
   const recipes = await getRecipes();
 
   return (
-   <main>
-    <div className="grid grid-cols-3 gap-8">
+  //  <main>
+  //   <div className="grid grid-cols-3 gap-8">
+  //   {recipes.map(recipe => (
+  //     <Card key={recipe.id} className='flex flex-col justify-between' >
+  //       <CardHeader className='flex-row gap-4 items-center'>
+  //         <Avatar>
+  //           <AvatarImage src={`/img/${recipe.image}`} alt='Recipe Image'/>
+  //           <AvatarFallback>
+  //             {recipe.title.slice(0, 2)}
+  //           </AvatarFallback>
+  //         </Avatar>
+  //         <div>
+  //           <CardTitle>
+  //             {recipe.title}
+  //           </CardTitle>
+  //           <CardDescription>
+  //             {recipe.time} Mins To Cook.
+  //           </CardDescription>
+  //         </div>
+  //       </CardHeader>
+  //       <CardContent>
+  //         <p>{recipe.description}</p>
+  //       </CardContent>
+  //       <CardFooter className='flex justify-between'>
+  //         <Button>View Recipe</Button>
+  //         {recipe.vegan ? 
+  //         <Badge variant="secondary">Vegan</Badge> :
+  //         <Badge variant="secondary">Not Vegan</Badge>}
+  //       </CardFooter>
+  //     </Card>
+  //   ) )}
+  //   </div>
+  //  </main>
+  <main>
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8">
     {recipes.map(recipe => (
       <Card key={recipe.id} className='flex flex-col justify-between' >
         <CardHeader className='flex-row gap-4 items-center'>
@@ -66,8 +99,9 @@ export default async function Home() {
           <Badge variant="secondary">Not Vegan</Badge>}
         </CardFooter>
       </Card>
-    ) )}
-    </div>
-   </main>
+    ))}
+  </div>
+</main>
+
   );
 };
